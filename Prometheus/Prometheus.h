9efd0d7eb232f24a541aa913b99eaf04ffe47bb1
Ibrahima Ciss <bionik6@gmail.com>
2019-02-15 12:00:51 +0000

    Initial Commit

---
+++ Prometheus/Prometheus.h
@@ -0,0 +1,19 @@
+//
+//  Prometheus.h
+//  Prometheus
+//
+//  Created by Ibrahima Ciss on 2/15/19.
+//  Copyright © 2019 Ibrahima Ciss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Prometheus.
+FOUNDATION_EXPORT double PrometheusVersionNumber;
+
+//! Project version string for Prometheus.
+FOUNDATION_EXPORT const unsigned char PrometheusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Prometheus/PublicHeader.h>
+
+
